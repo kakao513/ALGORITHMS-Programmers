91d018c2dbd398b1853b8b997ed3d9844a368977
Gunho <paulryu1998@gmail.com>
2023-05-05 17:00:31 +0100

    [DAY5]: Level2 19. 행렬의 곱셉

--- level2/19. 행렬의 곱셈.js
+++ level2/19. 행렬의 곱셈.js
@@ -1,8 +1,21 @@
 // 행렬의 곱셈
 
 function solution(arr1, arr2) {
-	var answer = [[]];
-	return answer;
-}
+	const result = [];
 
-console.log(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+	for (let i = 0; i < arr1.length; i++) {
+		const newArr = [];
+		let temp = 0;
+
+		for (let j = 0; j < arr2[0].length; j ++) {
+			for (let k = 0; k < arr2.length; k ++) {
+				temp += arr1[i][k] * arr2[k][j]
+			}
+			newArr.push(temp);
+			temp = 0;
+		}
+		result.push(newArr);
+	}
+
+	return result;
+}
